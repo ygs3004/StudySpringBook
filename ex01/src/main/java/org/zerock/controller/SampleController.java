153613cb9f9ffd445d20ad58511e1ab8cd94cc99
GunSoo Yoon <ygs3004@naver.com>
2022-09-21 17:47:09 +0900

    220921ac

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -1,6 +1,9 @@
 package org.zerock.controller;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
@@ -82,4 +85,31 @@ public class SampleController {
         return "/sample/ex04";
     }
 
+    @GetMapping("/ex05")
+    public void ex05(){
+        log.info("/ex05...............");
+    }
+
+    @GetMapping("/ex06")
+    public @ResponseBody SampleDTO ex06(){
+        log.info("/ex06...........");
+        SampleDTO dto = new SampleDTO();
+        dto.setAge(10);
+        dto.setName("홍길동");
+
+        return dto;
+    }
+
+    @GetMapping("/ex07")
+    public ResponseEntity<String> ex07(){
+        log.info("/ex07.................................");
+
+        // {"name":"홍길동"}
+        String msg="{\"name\":\"홍길동\"}";
+
+        HttpHeaders header = new HttpHeaders();
+        header.add("Content-Type", "application/json;charset=UTF-8");
+
+        return new ResponseEntity<>(msg, header, HttpStatus.OK);
+    }
 }
