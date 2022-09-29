27d421802287ea48c25c15e29be7051abb92467f
GunSoo Yoon <ygs3004@naver.com>
2022-09-29 21:21:48 +0900

    220929_repairJDBC

---
+++ jex02/src/main/resources/tbl_board.sql
@@ -0,0 +1,14 @@
+CREATE SEQUENCE seq_board;
+
+CREATE TABLE tbl_board(
+    bno NUMBER(10,0),
+    title VARCHAR2(200) not null,
+    content VARCHAR2(2000) not null,
+    writer VARCHAR2(50) not null,
+    regdate DATE DEFAULT SYSDATE,
+    updatedate DATE DEFAULT SYSDATE
+);
+
+ALTER TABLE tbl_board ADD CONSTRAINT pk_board PRIMARY KEY (bno);
+
+INSERT INTO tbl_board (bno, title, content, writer) VALUES (seq_board.nextval, '테스트 제목', '테스트 내용', 'user00');
