8bdeb38ff59f851e1a9d6509d420dac7802ac7f5
GunSoo Yoon <ygs3004@naver.com>
2022-10-12 03:03:28 +0900

    221012_0303

---
+++ ex06/src/main/resources/tbl_member.sql
@@ -0,0 +1,18 @@
+CREATE TABLE tbl_member(
+    userid VARCHAR2(50) NOT NULL PRIMARY KEY,
+    userpw VARCHAR2(100) not null,
+    username VARCHAR2(100) not null,
+    regdate DATE DEFAULT SYSDATE,
+    updatedate DATE DEFAULT SYSDATE,
+    enabled char(1) DEFAULT '1'
+);
+
+CREATE TABLE tbl_member_auth(
+    userid VARCHAR2(50) NOT NULL,
+    auth VARCHAR2(50) NOT NULL,
+    CONSTRAINT fk_member_auth FOREIGN KEY (userid) REFERENCES tbl_member(userid)
+
+);
+
+SELECT * FROM tbl_member;
+SELECT * FROM tbl_member_auth;
